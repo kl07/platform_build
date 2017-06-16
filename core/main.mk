35233adda7f3da5bd6eb8e5c8ee413b232d8fd4c
Daniel Micay <danielmicay@gmail.com>
2017-06-15 23:06:11 -0400

    show SELinux status in Settings device info
    Change-Id: I2858559cd33276b4be93fa4d019ca210a7b75850

--- core/main.mk
+++ core/main.mk
@@ -206,6 +206,9 @@ ADDITIONAL_BUILD_PROPERTIES += ro.treble.enabled=${PRODUCT_FULL_TREBLE}
 ### between the build variants
 ###
 
+# Show SELinux status in Settings device info
+ADDITIONAL_BUILD_PROPERTIES += ro.build.selinux=1
+
 is_sdk_build :=
 
 ifneq ($(filter sdk win_sdk sdk_addon,$(MAKECMDGOALS)),)
